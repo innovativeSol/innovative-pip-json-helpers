e92a00708d318fc9e8b7145f8f81ecd0913d79a5
Joshua Russo <josh.r.russo@gmail.com>
2022-04-20 15:15:56 -0400

    updated package shell

--- json_helpers/json_helpers.py
+++ json_helpers/json_helpers.py
@@ -1,1 +1,19 @@
 """Main module."""
+import json
+from datetime import datetime
+
+
+def datetime_str(value: datetime) -> str:
+    return value.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+
+def datetime_from_standard_str(value: str) -> datetime:
+    return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+
+class JSONEncoderWithDateHandling(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return datetime_str(obj)
+
+        return obj
